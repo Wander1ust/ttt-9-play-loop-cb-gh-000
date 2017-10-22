a24ea67d1b38e6116c9e4c705703c999d386be35
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-22 15:58:06 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,38 +1,52 @@
-# Helper Methods
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+  puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+    if index.between?(0,8) && !position_taken?(board, index)
+      true
+    end
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
   end
 end
 
-# Define your play method below
+def input_to_index(input)
+   s = input.to_i
+   s - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+def play(board)
+  puts "Please enter 1-9:"
+  i = gets.strip
+  index = input_to_index(i)
+  m = valid_move?(board, index)
+  if m == true
+    move(board, index, char="X")
+  else m == false
+    until m == true
+      puts "Sorry, that was an invalid move. Please enter 1-9:"
+      i = gets.strip
+      index = input_to_index(i)
+      m = valid_move?(board, index)
+      move(board, index, char="X")
+    end
+  end
+
+
+  end
+  display_board(board)
+end
